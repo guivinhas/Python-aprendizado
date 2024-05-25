3f09fd6c34d605fde7417c39a7563bb2cb7b8862
Guilherme Vinhas <100172462+guivinhas@users.noreply.github.com>
2024-05-25 00:04:47 -0300

    Operadores aritméticos

---
+++ ex002.py
@@ -0,0 +1,9 @@
+nome = 'Guilherme Vinhas'
+idade = 22
+altura = 1.82
+peso = 73
+imc = peso / (altura * altura)
+
+print(nome, 'tem', altura,'m de altura')
+print('Pesa', peso, 'KG e seu IMC é:')
+print(format(imc, '.2f'))
